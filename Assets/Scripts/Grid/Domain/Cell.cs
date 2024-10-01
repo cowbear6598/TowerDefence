360662425b667895c924f90b93d7abbbc8ccb298
CptSoap <cowbear6598@gmail.com>
2024-10-01 17:43:09 +0800

    :construction: complete cell and working on grid

---
+++ Assets/Scripts/Grid/Domain/Cell.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Grid.Domain
+{
+	public class Cell
+	{
+		public readonly Vector2Int Position;
+
+		private GameObject OccupyingObject;
+
+		public Cell(Vector2Int position)
+		{
+			Position = position;
+		}
+
+		public void SetOccupyingObject(GameObject occupyingObject) => OccupyingObject = occupyingObject;
+		public bool IsOccupied()                                   => OccupyingObject != null;
+	}
+}
---
+++ Assets/Scripts/Grid/Handlers/GridHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Grid.Handlers
+{
+	public class GridHandler
+	{
+
+
+		[Serializable]
+		public class Settings
+		{
+			public int Width;
+			public int Height;
+			public int CellSize;
+		}
+	}
+}
